3a20611c905ddb60d9699af168330405984169f6
Rafael Antonelli <rafaelantonelli5@hotmail.com>
2021-03-12 21:42:34 -0300

    projeto pedido

--- src/Enums/OrderStatus.java
+++ src/Enums/OrderStatus.java
@@ -1,5 +1,8 @@
 package Enums;
 
 public enum OrderStatus {
-
+	PENDING_PAYMENT,
+	PROCESSING,
+	SHIPPED,
+	DELIVERED;
 }
--- src/application/Exercicio_Pedido.java
+++ src/application/Exercicio_Pedido.java
@@ -5,33 +5,61 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 import java.util.Scanner;
+import java.util.TimeZone;
 
+import Enums.OrderStatus;
 import entities.Client;
+import entities.Order;
+import entities.OrderItem;
+import entities.Product;
 
 public class Exercicio_Pedido {
 
 	public static void main(String[] args) throws ParseException {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
+		Order order = new Order();
+		Date orderDate = new Date(System.currentTimeMillis());
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-
-		System.out.print("Enter cliente data:");
-		System.out.print("Name: ");
-		
+		SimpleDateFormat sdf2 = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 	
-		System.out.print("Enter cliente data:");
+		System.out.println("Enter cliente data:");
 		System.out.print("Name: ");
 		String name = sc.nextLine();
 		System.out.print("Email: ");
 		String email = sc.next();
 		System.out.print("Birth date (DD/MM/YYYY): ");
-		Date Client = sdf.parse(sc.next());
+		Date birthDate = sdf.parse(sc.next());
+		Client client = new Client(name, email, birthDate);
 		
-		Client client = new Client(name, email, Client);
-		
-		System.out.println("Enter order data:");
+		System.out.println("Enter order data: ");
+		System.out.print("Status: ");
+		OrderStatus os1 = OrderStatus.valueOf(sc.next());
+		System.out.print("How many items to this order? ");
 		int n = sc.nextInt();
-
-	}
+		
+		 for (int i = 1; i<=n; i++) {
+			System.out.println("Enter #" + i + " item data: ");
+			System.out.print("Product name: ");
+			String nameP = sc.next();
+			System.out.print("Product price: ");
+			double price = sc.nextDouble();
+			System.out.println("Quantity: ");
+			int quantity = sc.nextInt();
+			OrderItem item = new OrderItem(quantity, price, new Product(nameP, price));
+			order.addItem(item);
+		}
+		
+		System.out.println("ORDER SUMARY");
+		System.out.println("Order moment: " + sdf2.format(orderDate));
+		System.out.println("Order status: " + OrderStatus.PROCESSING);
+		System.out.println("Client: " + client.getName() + ", " + sdf.format(client.getBirthDate())  + ", " + client.getEmail());
+		System.out.println("Order Items: ");
+		System.out.println(order.total());
+		
+		
+		
+		sc.close();
+	} 
 
 }
